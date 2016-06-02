909e5859add9018206a9d8f9cfc031203e73c948
Joseph McCausland <jmccausland@esri.com>
2016-06-02 09:19:58 -0400

    need to change branch

---
+++ source/CoordinateConversion/ProAppCoordConversionModule.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArcGIS.Core.Hosting;
+using CoordinateConversionLibrary.Models;
+
+namespace ProAppCoordConversionModule.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod, STAThread]
+        public void TestProCoordinateGet()
+        {
+            Host.Initialize();
+
+            var proGetter = new ProCoordinateGet();
+
+            var getBase = proGetter as CoordinateGetBase;
+
+            getBase.InputCoordinate = "44.123 -121.456";
+
+            string coord = string.Empty;
+            var result = proGetter.CanGetDD(4326, out coord);
+            Assert.IsTrue(result);
+        }
+    }
+}
